fed7370f804c9f4a0560e50711e5301bb4395f79
Giridhar Kolli <Giridhar.Kolli@lvs1.com>
2022-03-14 22:16:21 -0700

    remove service delegation for subnet

--- test/fixtures/resources/main.tf
+++ test/fixtures/resources/main.tf
@@ -40,18 +40,4 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.resource_group.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
-
-  enforce_private_link_endpoint_network_policies = true
-  enforce_private_link_service_network_policies  = false
-
-  delegation {
-    name = "Microsoft.ContainerInstance"
-
-    service_delegation {
-      name    = "Microsoft.ContainerInstance/containerGroups"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-    }
-
-  }
-
 }
