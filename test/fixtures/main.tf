c9b01987d00cad896991ebdcb74fc8608e3cd9b8
Giridhar Kolli <Giridhar.Kolli@lvs1.com>
2023-06-20 13:39:32 -0700

    updated changes

--- test/fixtures/main.tf
+++ test/fixtures/main.tf
@@ -24,7 +24,7 @@ module "firewall" {
   resource_group_name = module.networking.virtual_network_resource_group_name
   sku_name            = "AZFW_VNet"
   sku_tier            = "Standard"
-  firewall_policy_id  = var.firewall_policy_id
+  firewall_policy_id  = module.networking.firewall_policy_id
 
 
   network_rule_collections = [
--- test/fixtures/provider.tf
+++ test/fixtures/provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.99.0"
+      version = ">= 2.88.1, < 3.0.0"
     }
   }
 }
@@ -11,4 +11,3 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
