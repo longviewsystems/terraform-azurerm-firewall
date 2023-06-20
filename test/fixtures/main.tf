7328319fb706cd9ebe9f956b0b98f7bf0e116962
Giridhar Kolli <Giridhar.Kolli@lvs1.com>
2023-06-20 13:00:59 -0700

    updated fw changes

--- test/fixtures/main.tf
+++ test/fixtures/main.tf
@@ -24,7 +24,6 @@ module "firewall" {
   resource_group_name = module.networking.virtual_network_resource_group_name
   sku_name            = "AZFW_VNet"
   sku_tier            = "Standard"
-  firewall_policy_id  = module.naming.firewall_policy_id
 
   network_rule_collections = [
     {
